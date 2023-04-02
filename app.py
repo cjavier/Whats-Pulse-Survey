dab0887e71a766a21d0caebf16f67ca4eaf8fc73
Javier <cjavier.gar@gmail.com>
2023-04-01 20:59:33 -0600

    tutorial de meta hasta el mensaje prueba

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
-
+import json
+from flask import Flask, render_template
+import flask
+from message_helper import get_text_message_input, send_message
+ 
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+ 
+with open('config.json') as f:
+    config = json.load(f)
+ 
+app.config.update(config)
+ 
+@app.route("/")
+def index():
+    return render_template('index.html', name=__name__)
+ 
+@app.route('/welcome', methods=['POST'])
+async def welcome():
+  data = get_text_message_input(app.config['RECIPIENT_WAID']
+                                , 'Welcome to the Flight Confirmation Demo App for Python!');
+  await send_message(data)
+  return flask.redirect(flask.url_for('index'))
